33ebac5b648751390d0c4987769b744f9052cd79
germanUbuntu <germanjimenezz18@gmail.com>
2024-08-13 19:33:50 +0200

    Account CRUD Complete

--- api.go
+++ api.go
@@ -5,14 +5,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
+
+type APIServer struct {
+	listenAddr string
+	store      Storage
+}
 
 func (s *APIServer) Run() {
 	router := mux.NewRouter()
 	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
 	router.HandleFunc("/account/{id}", makeHTTPHandleFunc(s.handleGetAccountByID))
+	router.HandleFunc("/transfer", makeHTTPHandleFunc(s.handleTransfer))
 
 	log.Println("JSON API server running on port:" + s.listenAddr)
 	http.ListenAndServe(s.listenAddr, router)
@@ -25,23 +32,43 @@ func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error 
 	if r.Method == "POST" {
 		return s.handleCreateAccount(w, r)
 	}
-	if r.Method == "DELETE" {
-		return s.handleDeleteAccount(w, r)
-	}
 
 	return fmt.Errorf("method now allowed %s", r.Method)
 }
 
+// GET  /account
 func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
+	accounts, err := s.store.GetAccounts()
 
-	return nil
+	if err != nil {
+		return err
+	}
+
+	return WriteJSON(w, http.StatusOK, accounts)
 }
+
 func (s *APIServer) handleGetAccountByID(w http.ResponseWriter, r *http.Request) error {
-	vars := mux.Vars(r)["id"]
-	fmt.Println(vars)
-	// account := NewAccount("German", "GG")
-	return WriteJSON(w, http.StatusOK, &Account{})
+
+	if r.Method == "GET" {
+		id, err := getID(r)
+		if err != nil {
+			return err
+		}
+
+		account, err := s.store.GetAccountByID(id)
+		if err != nil {
+			return err
+		}
+		return WriteJSON(w, http.StatusOK, account)
+	}
+
+	if r.Method == "DELETE" {
+		return s.handleDeleteAccount(w, r)
+	}
+
+	return fmt.Errorf("method not allowed %s", r.Method)
 }
+
 func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
 	createAccountRequest := new(CreateAccountRequest)
 
@@ -57,10 +84,24 @@ func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) 
 	return WriteJSON(w, http.StatusOK, account)
 }
 func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
-	return nil
+	id, err := getID(r)
+	if err != nil {
+		return err
+	}
+
+	if err := s.store.DeleteAccount(id); err != nil {
+		return err
+	}
+
+	return WriteJSON(w, http.StatusOK, map[string]int{"deleted": id})
 }
 func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
-	return nil
+	transferReq := new(TransferRequest)
+	if err := json.NewDecoder(r.Body).Decode(transferReq); err != nil {
+		return err
+	}
+	defer r.Body.Close()
+	return WriteJSON(w, http.StatusOK, transferReq)
 }
 
 func WriteJSON(w http.ResponseWriter, status int, v any) error {
@@ -69,13 +110,9 @@ func WriteJSON(w http.ResponseWriter, status int, v any) error {
 	return json.NewEncoder(w).Encode(v)
 }
 
-type APIServer struct {
-	listenAddr string
-	store      Storage
-}
 type apiFunc func(http.ResponseWriter, *http.Request) error
 type ApiError struct {
-	Error string
+	Error string `json:"error"`
 }
 
 func NewAPIServer(listenAddr string, store Storage) *APIServer {
@@ -88,4 +125,13 @@ func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
 			WriteJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
 		}
 	}
+}
+
+func getID(r *http.Request) (int, error) {
+	idStr := mux.Vars(r)["id"]
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		return id, fmt.Errorf("invalid id given %s", idStr)
+	}
+	return id, nil
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,26 @@
-$ docker run --name some-postgres -e POSTGRES_PASSWORD=mysecretpassword -d postgres
+# Guía de Instalación de PostgreSQL usando Docker
+
+Este documento proporciona instrucciones para ejecutar una instancia de PostgreSQL en un contenedor Docker.
+
+## Prerrequisitos
+
+Antes de comenzar, asegúrate de tener instalado Docker en tu sistema. Si no lo tienes, puedes seguir las instrucciones de instalación en la [documentación oficial de Docker](https://docs.docker.com/get-docker/).
+
+## Instalación
+
+Para desplegar PostgreSQL en un contenedor Docker, sigue los siguientes pasos:
+
+### 1. Ejecutar el contenedor de PostgreSQL
+
+Para iniciar un contenedor de PostgreSQL, utiliza el siguiente comando:
+
+```bash
+docker run --name some-postgres -e POSTGRES_PASSWORD=1124 -p 5432:5432 -d postgres
+```
+
+ ### 2. Testear contenedor
+```bash
+docker ps
+```
+
+ 
